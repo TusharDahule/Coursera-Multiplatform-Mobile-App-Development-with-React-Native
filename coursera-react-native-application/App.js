c8ef6dd246ddb30b32d4696edbb90bc17e9cb264
Tushar Dahule <43333828+TusharDahule@users.noreply.github.com>
2020-06-26 01:26:11 +0530

    Add files via upload

---
+++ coursera-react-native-application/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react'
+
+import Main from './components/MainComponent';
+import { Loading } from './components/LoadingComponent';
+
+import { ConfigureStore } from './redux/configureStore';
+
+const { persistor, store } = ConfigureStore();
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate
+          loading={<Loading />}
+          persistor={persistor}>
+          <Main />
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
